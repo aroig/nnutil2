33ba0a06b86289f180398c83b7954eb3d44749ed
Abdo Roig-Maranges <abdo.roig@gmail.com>
2019-12-27 18:59:18 +0100

    add support for depthwie convolutional layers

--- nnutil2/layers/conv.py
+++ nnutil2/layers/conv.py
@@ -20,28 +20,19 @@ class Conv(Layer):
     """Convolutional layer wrapper that chooses the right kernel dimension
        based on input shape
     """
-    def __init__(self, input_shape=None, separable=False, *args, **kwargs):
+    def __init__(self, input_shape=None, mode='full', *args, **kwargs):
         assert input_shape is not None
         self._in_shape = as_shape(input_shape)
-        Layer = self._layer_class(input_shape, separable)
 
+        self._mode = mode
+
+        Layer = self._layer_class(input_shape, mode)
         self._layer = Layer(*args, input_shape=input_shape, **kwargs)
+
         super(Conv, self).__init__()
 
-    def _layer_class(self, shape, separable):
-        if separable:
-            if self._in_shape.rank == 1:
-                return tf.keras.layers.Dense
-
-            elif self._in_shape.rank == 2:
-                return tf.keras.layers.SeparableConv1D
-
-            elif self._in_shape.rank == 3:
-                return tf.keras.layers.SeparableConv2D
-
-            else:
-                raise Exception("Input shape must have rank between 1 and 3 for separable convolutions")
-        else:
+    def _layer_class(self, shape, mode):
+        if mode == 'full':
             if self._in_shape.rank == 1:
                 return tf.keras.layers.Dense
 
@@ -56,6 +47,29 @@ class Conv(Layer):
 
             else:
                 raise Exception("Input shape must have rank between 1 and 4 for convolutions")
+
+        elif mode == 'separable':
+            if self._in_shape.rank == 1:
+                return tf.keras.layers.Dense
+
+            elif self._in_shape.rank == 2:
+                return tf.keras.layers.SeparableConv1D
+
+            elif self._in_shape.rank == 3:
+                return tf.keras.layers.SeparableConv2D
+
+            else:
+                raise Exception("Input shape must have rank between 1 and 3 for separable convolutions")
+
+        elif mode == 'depthwise':
+            if self._in_shape.rank == 2:
+                return tf.keras.layers.DepthwiseConv2D
+
+            else:
+                raise Exception("Input shape must have rank 3 for depthwise convolutions")
+
+        else:
+            raise Exception("Unrecognized convolution mode".format(mode))
 
     def get_config(self):
         return self._layer.get_config()
--- tests/layers_conv.py
+++ tests/layers_conv.py
@@ -17,7 +17,7 @@ import tensorflow as tf
 import nnutil2 as nnu
 
 class LayersConv(tf.test.TestCase):
-    def test_layer_shape_1D(self):
+    def test_layer_full_conv_1D(self):
         with self.cached_session() as sess:
             shape = (5, 1)
             x = tf.random.normal(shape=(4,) + shape, dtype=tf.float32)
@@ -32,7 +32,7 @@ class LayersConv(tf.test.TestCase):
             y = layer(x)
             self.assertEqual(tf.TensorShape([4, 3, 8]), y.shape)
 
-    def test_layer_shape_2D(self):
+    def test_layer_full_conv_2D(self):
         with self.cached_session() as sess:
             shape = (5, 5, 1)
             x = tf.random.normal(shape=(4,) + shape, dtype=tf.float32)
@@ -47,7 +47,7 @@ class LayersConv(tf.test.TestCase):
             y = layer(x)
             self.assertEqual(tf.TensorShape([4, 3, 3, 8]), y.shape)
 
-    def test_layer_shape_3D(self):
+    def test_layer_full_conv_3D(self):
         with self.cached_session() as sess:
             shape = (5, 5, 5, 1)
             x = tf.random.normal(shape=(4,) + shape, dtype=tf.float32)
@@ -61,6 +61,54 @@ class LayersConv(tf.test.TestCase):
 
             y = layer(x)
             self.assertEqual(tf.TensorShape([4, 3, 3, 3, 8]), y.shape)
+
+    def test_layer_separable_conv_1D(self):
+        with self.cached_session() as sess:
+            shape = (5, 1)
+            x = tf.random.normal(shape=(4,) + shape, dtype=tf.float32)
+
+            layer = nnu.layers.Conv(
+                input_shape=shape,
+                mode='separable'
+                filters=8,
+                kernel_size=(3,),
+                activation=tf.keras.activations.relu
+            )
+
+            y = layer(x)
+            self.assertEqual(tf.TensorShape([4, 3, 8]), y.shape)
+
+    def test_layer_separable_conv_2D(self):
+        with self.cached_session() as sess:
+            shape = (5, 5, 1)
+            x = tf.random.normal(shape=(4,) + shape, dtype=tf.float32)
+
+            layer = nnu.layers.Conv(
+                input_shape=shape,
+                mode='separable'
+                filters=8,
+                kernel_size=(3, 3),
+                activation=tf.keras.activations.relu
+            )
+
+            y = layer(x)
+            self.assertEqual(tf.TensorShape([4, 3, 3, 8]), y.shape)
+
+    def test_layer_depthwise_conv_2D(self):
+        with self.cached_session() as sess:
+            shape = (5, 5, 1)
+            x = tf.random.normal(shape=(4,) + shape, dtype=tf.float32)
+
+            layer = nnu.layers.Conv(
+                input_shape=shape,
+                mode='depthwise'
+                filters=8,
+                kernel_size=(3, 3),
+                activation=tf.keras.activations.relu
+            )
+
+            y = layer(x)
+            self.assertEqual(tf.TensorShape([4, 3, 3, 8]), y.shape)
 
 
 if __name__ == '__main__':
